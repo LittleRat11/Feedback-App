f2dbf9e35ac0dc5ba3aafe05eb11550cb23dfe1c
min khant <minkhant4287@gmail.com>
2024-03-22 21:39:58 +0630

    Added my first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const rating = document.querySelectorAll(".rating");
+const btn = document.querySelector("#btn");
+const container = document.querySelector("#container");
+let selectedRating = "";
+
+rating.forEach((rating) => {
+    rating.addEventListener("click", (event) => {
+        //emoji ko click yin value ya chin loh 
+        // console.log(event.target.innerText || event.target.parentNode.innerText);
+        //active ko remove pyn lote 
+        removeActive();
+        selectedRating =
+            event.target.innerText || event.target.parentNode.innerText;
+        //active class add chin
+        event.target.classList.add("active")
+        event.target.parentNode.classList.add("active")
+
+    });
+});
+
+btn.addEventListener("click", () => {
+    if (selectedRating != "") {
+        container.innerHTML = `
+        <strong>Thank You!</strong>
+        <br/>
+        <br/>
+        <strong>Feedback =  ${selectedRating}</strong>
+        <p>We'll use your feedback to improve our customer support.</p>
+        `
+    }
+})
+
+function removeActive() {
+    rating.forEach((rating) => {
+        rating.classList.remove("active");
+    });
+}
